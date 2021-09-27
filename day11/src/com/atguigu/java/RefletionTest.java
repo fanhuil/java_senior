33460b59aa2cfef1f9ac3ea0a2d1b1493f8f73bb
fanhuilin <fanhuilinl@163.com>
2021-09-27 23:30:45 +0800

    注解入门

--- day11/src/com/atguigu/java/RefletionTest.java
+++ day11/src/com/atguigu/java/RefletionTest.java
@@ -1,15 +1,22 @@
 package com.atguigu.java;
 
 /**
+ * 注解的使用
+ * 1、理解Annotation
+ * 2、使用示例
+ *      生成文档的相关注解
+ *
+ *
  * @auchor fanhuilin
  * @create 2021 上午 10:38
  */
 public class RefletionTest {
     public static void main(String[] args) {
-        // 反射之前，对于Person的操作
-        @Test
-        public void test1 () {
 
-        }
     }
+
+    public void a(){
+
+    }
+
 }
