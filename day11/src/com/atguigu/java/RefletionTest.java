693b2d26f1191e0e1cf1ad1c13858c1b87c8e7b2
DESKTOP-S26BPHO\admin <fanhuilinl@163.com>
2021-09-27 18:06:49 +0800

    第一次提交

---
+++ day11/src/com/atguigu/java/RefletionTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.java;
+
+/**
+ * @auchor fanhuilin
+ * @create 2021 上午 10:38
+ */
+public class RefletionTest {
+    public static void main(String[] args) {
+        // 反射之前，对于Person的操作
+        @Test
+        public void test1 () {
+
+        }
+    }
+}
